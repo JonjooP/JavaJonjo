74bc17a510b4ec9b766f4949cf400a4878793573
JonjooP <jcpheiffer@gmail.com>
2024-02-23 12:34:46 +0200

    Javascript training in progress

---
+++ Training/checkio.js
@@ -0,0 +1,28 @@
+/*
+function zanjo() {                                  //typical function
+    console.log("Jonny Pheiffer")
+}
+zanjo()
+*/
+/*
+function functionWithArgs(h, p) {           //functions with arguments
+    console.log(h * p);
+  }
+  functionWithArgs(4, 20)
+  */
+/*
+  function timesFive(bum) {                 //will get back to his arg, kinda understand
+    return bum * 5;
+  }
+  
+  const answer = timesFive(10);
+*/
+
+let processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+processed = processArg(7)
+
+alert
