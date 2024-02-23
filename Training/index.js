2d2c872dcd2bf126627ae69f8df13f9f8250b6a7
JonjooP <jcpheiffer@gmail.com>
2024-02-23 12:32:46 +0200

    javascript,css and html. Step 116 of RPG game project and some random .js files for testing

---
+++ Training/index.js
@@ -0,0 +1,10 @@
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = "The really " +  myAdjective + " " + "brownish " + myNoun + " cowardly " + myVerb + " over our grass " + myAdverb + " all day " + "."; // Change this line
+// Only change code above this line
+
+console.log (wordBlanks)
--- Training/jon.js
+++ Training/jon.js
@@ -1,3 +1,38 @@
-let camper = "James";
-let camper = "David";
-console.log(camper)
+ // tested in-line comment
+
+/* the is the test of 
+    A MULTI-LINE COMMENT*/
+    
+    /* my first function to fill in missing words and add spaces to concatenated sentences
+    function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+        var result = "";
+        result += "The really " + myAdjective + " " + "brownish " + " " + myNoun + " " + "cowardly " + myVerb + " " + "over our grass " + myAdverb + " " + "all day " + ".";
+        return result;
+    }
+
+    console.log(wordBlanks("dog", "big", "ran", "quickly"));
+    console.log(wordBlanks("robot", "rusted", "danced", "silly"));
+
+    */
+/* selecting an index of the array element
+    const ourArray = [100,500,1000,5050]
+    var myData = ourArray[2]
+    console.log(myData)
+
+    */
+    /* push new element to existing array as they are mutible
+    const myArray = [["John", 23], ["cat", 2]];  //Push ["dog", 3] onto the end of the myArray variable.
+    myArray.push(["dog, 3"]);
+    console.log(myArray);*/
+
+
+    /* .pop() is used to pop a value off of the end of an array.
+    const threeArr = [1, 4, 6];
+const oneDown = threeArr.pop();
+console.log(oneDown);     //shows 6
+console.log(threeArr);     //shows remaining array index in element
+*/
+
+const numbers = [1, 2, 3];
+const firstNumber = numbers.shift();
+console.log(firstNumber);
